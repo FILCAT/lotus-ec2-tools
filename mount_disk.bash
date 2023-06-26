2384de08cbe3cfc1ba3b177721d10a98a14be7b2
Mikers <snissn@gmail.com>
2023-06-26 11:35:21 -1000

    mount idsk

---
+++ mount_disk.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the disk device name and mount point
+disk_device="/dev/nvme0n1"
+mount_point="/mnt/data"
+
+# Format the disk with the Ext4 file system
+sudo mkfs -t ext4 $disk_device
+
+# Create the mount point directory if it doesn't exist
+sudo mkdir -p $mount_point
+
+# Mount the disk
+sudo mount $disk_device $mount_point
+
+# Add an entry to /etc/fstab for automatic mounting on boot
+echo "$disk_device   $mount_point   ext4   defaults,nofail   0   2" | sudo tee -a /etc/fstab
+
