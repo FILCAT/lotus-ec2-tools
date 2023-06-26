867ee0c4fb903edf977603363f0a761183925186
Mikers <snissn@gmail.com>
2023-06-26 11:39:53 -1000

    mnt folder

--- download_snapshot.bash
+++ download_snapshot.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eux
-aria2c -x5 https://snapshots.mainnet.filops.net/minimal/latest.zst -o latest.zst
+aria2c -x5 https://snapshots.mainnet.filops.net/minimal/latest.zst -o /mnt/data/latest.zst
