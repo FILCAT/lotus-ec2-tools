8c48903c17fe3004dc5320536576883c26969c67
Mikers <snissn@gmail.com>
2023-06-26 11:31:20 -1000

    setup

---
+++ clone_repo.bash
@@ -0,0 +1,3 @@
+
+git clone https://github.com/snissn/state-invariants-check.git
+cd state-invariants-check/
---
+++ setup.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+#run this as root
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt install -y hwloc mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y 
+apt upgrade -y
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+GO_VERSION_MIN=$(curl https://raw.githubusercontent.com/filecoin-project/lotus/master/GO_VERSION_MIN)
+wget -c https://golang.org/dl/go$GO_VERSION_MIN.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
