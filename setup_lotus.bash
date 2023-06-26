aa54045a18addf93d716b21d35e31c16a4be932c
Mikers <snissn@gmail.com>
2023-06-26 11:48:06 -1000

    progress

--- setup_lotus.bash
+++ setup_lotus.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eux
-#todo install lotus build requirements
-git clone git@github.com:filecoin-project/lotus.git 
+git clone https://github.com/filecoin-project/lotus.git
 cd lotus
 make lotus lotus-shed
