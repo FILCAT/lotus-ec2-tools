4d2cc4d03ecff62e0126bf5ce3df86203ee6e71b
mike seiler <snissn@gmail.com>
2023-06-28 13:15:17 -0700

    dir for lotus

--- import_snapshot.bash
+++ import_snapshot.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eux
+lotus_dir=/mnt/data/state-invariants-check/lotus
 export LOTUS_PATH=/mnt/data/lotus
-./lotus/lotus daemon --halt-after-import --import-snapshot  /mnt/data/latest.zst
+$lotus_dir/lotus/lotus daemon --halt-after-import --import-snapshot  /mnt/data/latest.zst
