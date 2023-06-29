b461de251833f21e6ca4f8ca804b3b542bcf1d25
Ubuntu <ubuntu@ip-172-31-12-246.us-west-1.compute.internal>
2023-06-29 23:59:23 +0000

    fix

--- tasks/snapshot_state_summary/run_snapshot_state_summary.bash
+++ tasks/snapshot_state_summary/run_snapshot_state_summary.bash
@@ -3,4 +3,4 @@ set -eux
 
 export LOTUS_PATH=/mnt/data/lotus
 
-/mnt/data/lotus/lotus-shed --repo=$LOTUS_PATH stat-snapshot
+/mnt/data/state-invariants-check/lotus/lotus-shed --repo=$LOTUS_PATH stat-snapshot
