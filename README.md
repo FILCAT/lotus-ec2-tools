e0110eab8fa9903a5670e921b160060b5bd1777d
Ubuntu <ubuntu@ip-172-31-12-246.us-west-1.compute.internal>
2023-06-29 23:19:01 +0000

    server fixes and run instructions

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # state-invariants-check
+
+to set up the commander node:
+
+1. in util run create_instance.bash
+2. on the new node run aws configure
+3. set up pem in ~/.ssh/aws.pem
+4. add following to ~/.ssh/config:
+
+Host *
+  StrictHostKeyChecking no
--- tasks/state_invariants_check/run.bash
+++ tasks/state_invariants_check/run.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
+INSTANCE_PUBLIC_DNS="$1"
+
 new_file="check-invariants-`date +"%Y-%m-%d-%H-%M-%S"`.out"
-ssh -i  ~/Downloads/mikers-2.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/data/state-invariants-check/tasks/state_invariants_check/run_state_invariants_check.bash $new_file
-scp -i  ~/Downloads/mikers-2.pem  ubuntu@$INSTANCE_PUBLIC_DNS:/mnt/data/$new_file .
+ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/data/state-invariants-check/tasks/state_invariants_check/run_state_invariants_check.bash $new_file
+scp -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS:/mnt/data/$new_file .
