546ec143cc4dd84931a3821a034354f3c5f49c76
Ubuntu <ubuntu@ip-172-31-12-246.us-west-1.compute.internal>
2023-06-30 00:02:07 +0000

    temporary change branch and recompile

--- tasks/snapshot_state_summary/run_snapshot_state_summary.bash
+++ tasks/snapshot_state_summary/run_snapshot_state_summary.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eux
 
+#Temporyary until branch is merged merge:
+cd /mnt/data/state-invariants-check/lotus/
+git checkout feat/stat-snapshot
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc && source ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin
+mkdir -p /mnt/data/tmp
+export TMPDIR=/mnt/data/tmp
+make lotus lotus-shed
+
+
 export LOTUS_PATH=/mnt/data/lotus
 
 /mnt/data/state-invariants-check/lotus/lotus-shed --repo=$LOTUS_PATH stat-snapshot
