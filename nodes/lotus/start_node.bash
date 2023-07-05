9cf2f848fd02275ac23bf6c3e15b7df25b538cfc
Ubuntu <ubuntu@ip-172-31-14-137.us-west-1.compute.internal>
2023-07-05 22:43:17 +0000

    fixes

--- nodes/lotus/start_node.bash
+++ nodes/lotus/start_node.bash
@@ -6,6 +6,6 @@ dir=/mnt/data/state-invariants-check/nodes/lotus
 sudo cp $dir/lotus.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable filecoin.service
-sudo systemctl start filecoin.service
+sudo systemctl enable lotus.service
+sudo systemctl start lotus.service
 
