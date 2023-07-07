d4b684254320814bdff9b88cddece0111a9c7d6e
Ubuntu <ubuntu@ip-172-31-12-246.us-west-1.compute.internal>
2023-07-07 07:32:17 +0000

    slack hook import

--- tasks/snapshot_state_summary/run.bash
+++ tasks/snapshot_state_summary/run.bash
@@ -2,6 +2,7 @@
 set -eux
 
 INSTANCE_PUBLIC_DNS="$1"
+source /mnt/data/state-invariants-check/.env
 
 new_file="snapshot_state_summary-`date +"%Y-%m-%d-%H-%M-%S"`.out"
 ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/data/state-invariants-check/tasks/snapshot_state_summary/run_snapshot_state_summary.bash >  $new_file
