bf4aa4e719c2066afd2083dc144c423865c48a76
Mikers <snissn@gmail.com>
2023-07-25 14:08:27 -1000

    Customize profile_message_execution task from template
    The previous commit added the profile_message_execution task based on a template by running the add_task.sh script. This commit provides customizations to the initial template.
    
    The output handling and data retrieval method of the run bash script has been modified. Instead of redirecting the output to a new file, the script is now executed directly via SSH. Additionally, instead of outputting to a single file, a new directory is created based on the current date and time, and two profiling SVGs are copied from the remote server to this new directory using SCP.
    
    The run_task bash script, rather than echoing a placeholder message, now executes the lotus daemon with a ten-minute time limit. Two different pprof commands are run to generate detailed profiling data. These modifications enhance the quality and granularity of the profiling data generated by the task.

--- tasks/profile_message_execution/run.bash
+++ tasks/profile_message_execution/run.bash
@@ -7,5 +7,12 @@ task_name="$2"
 
 mkdir -p /mnt/lotus-ec2-tools/tasks/$task_name/data/
 
-new_file="${task_name}-`date +"%Y-%m-%d-%H-%M-%S"`.out"
-ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/lotus-ec2-tools/tasks/${task_name}/run_${task_name}.bash >  /mnt/lotus-ec2-tools/tasks/${task_name}/data/$new_file
+ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/lotus-ec2-tools/tasks/${task_name}/run_${task_name}.bash
+
+
+new_dir="${task_name}-`date +"%Y-%m-%d-%H-%M-%S"`"
+
+new_dir_full=/mnt/lotus-ec2-tools/tasks/${task_name}/data/$new_dir
+
+scp -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS:profile001.svg $new_dir_full
+scp -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS:profile002.svg $new_dir_full
--- tasks/profile_message_execution/run_task.bash
+++ tasks/profile_message_execution/run_task.bash
@@ -2,6 +2,14 @@
 set -eux
 export LOTUS_PATH=/mnt/lotus-data
 
-#Add your code here to execute your new task
-#Pass data back through stdout
-echo 'do stuff here'
+# we have just synced the chain, now we are behind and will be catching up
+# profile the next ten minutes of cpu time
+# (ten minutes was chosen a balance of not taking too long and getting enough consistent data)
+timeout 10m  ./lotus daemon   --pprof pprof.out
+
+# could add more focus areas here if req'd
+go tool pprof -svg -focus=ApplyMessage -relative_percentages pprof.out
+go tool pprof -svg -nodecount=500 -relative_percentages pprof.out
+# this creates files
+# profile001.svg  and
+# profile002.svg which we will scp over
