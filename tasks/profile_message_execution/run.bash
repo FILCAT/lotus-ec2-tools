bc38eb1fdcce32411938210eebb7c1d8818ac176
Mike Seiler <snissn@gmail.com>
2023-07-26 06:47:18 +0000

    fix pathname

--- tasks/profile_message_execution/run.bash
+++ tasks/profile_message_execution/run.bash
@@ -7,7 +7,7 @@ task_name="$2"
 
 mkdir -p /mnt/lotus-ec2-tools/tasks/$task_name/data/
 
-ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/lotus-ec2-tools/tasks/${task_name}/run_${task_name}.bash
+ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/lotus-ec2-tools/tasks/${task_name}/run_task.bash
 
 
 new_dir="${task_name}-`date +"%Y-%m-%d-%H-%M-%S"`"
--- tasks/template/run.bash
+++ tasks/template/run.bash
@@ -8,4 +8,4 @@ task_name="$2"
 mkdir -p /mnt/lotus-ec2-tools/tasks/$task_name/data/
 
 new_file="${task_name}-`date +"%Y-%m-%d-%H-%M-%S"`.out"
-ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/lotus-ec2-tools/tasks/${task_name}/run_${task_name}.bash >  /mnt/lotus-ec2-tools/tasks/${task_name}/data/$new_file
+ssh -i  ~/.ssh/aws.pem  ubuntu@$INSTANCE_PUBLIC_DNS bash /mnt/lotus-ec2-tools/tasks/${task_name}/run_task.bash >  /mnt/lotus-ec2-tools/tasks/${task_name}/data/$new_file
