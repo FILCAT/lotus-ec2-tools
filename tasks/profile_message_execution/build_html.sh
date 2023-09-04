82757478b2a160524a35e016e47cc6e02713774a
Mikers <snissn@gmail.com>
2023-09-04 13:00:04 -1000

    reorganize template

--- tasks/profile_message_execution/build_html.sh
+++ tasks/profile_message_execution/build_html.sh
@@ -48,28 +48,27 @@ echo "<!DOCTYPE html>
         <h1>Lotus Profiling Report - Message Execution</h1>
         <p>This data was collected by preparing a new Lotus node by downloading a mainnet snapshot, then running Lotus with the pprof flag. The output was analyzed after the node ran for 10 minutes while syncing mainnet.</p>" > index.html
 
+echo "<h2>Overall Profiling Snapshot of Lotus Main Loop</h2>" >> index.html
+echo "<p>This SVG visualizes a profiling snapshot of the entire execution loop of the Lotus function, with a focus on the top 500 nodes for more manageable visualization.</p>" >> index.html
+echo "<div class=\"svg-container\">" >> index.html
+sed '/^<svg /s/\(width\|height\)="[^"]*"//g' profile002.svg >> index.html
+echo "</div>" >> index.html
+
+
 echo "<h2>Apply Message Function Profiling</h2>" >> index.html
 echo "<p>This section contains the profiling data of the ApplyMessage function, depicted in the form of a list from the profiling output.</p>" >> index.html
 echo "<pre>" >> index.html
 cat ApplyMessage.txt >> index.html
 echo "</pre>" >> index.html
 
-echo "<h2>Apply Message Relative Percentages</h2>" >> index.html
 echo "<p>This section provides the top relative percentages of the ApplyMessage function from the profiling output.</p>" >> index.html
 echo "<pre>" >> index.html
 cat RelativePercentages.txt >> index.html
 echo "</pre>" >> index.html
 
-echo "<h2>SVG Profile for Apply Message Function</h2>" >> index.html
 echo "<p>This SVG provides a graphical representation of the profiling data for the ApplyMessage function.</p>" >> index.html
 echo "<div class=\"svg-container\">" >> index.html
 sed '/^<svg /s/\(width\|height\)="[^"]*"//g' profile001.svg >> index.html
-echo "</div>" >> index.html
-
-echo "<h2>Overall Profiling Snapshot of Lotus Main Loop</h2>" >> index.html
-echo "<p>This SVG visualizes a profiling snapshot of the entire execution loop of the Lotus function, with a focus on the top 500 nodes for more manageable visualization.</p>" >> index.html
-echo "<div class=\"svg-container\">" >> index.html
-sed '/^<svg /s/\(width\|height\)="[^"]*"//g' profile002.svg >> index.html
 echo "</div>" >> index.html
 
 echo "</div>
