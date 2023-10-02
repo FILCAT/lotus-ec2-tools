e65b5913c4031797163c3508dc094a3a31cfa744
Mike Seiler <snissn@gmail.com>
2023-10-02 20:38:03 +0000

    fix lotus import command changing

--- setup/import_snapshot.bash
+++ setup/import_snapshot.bash
@@ -2,4 +2,4 @@
 set -eux
 lotus_dir=/mnt/lotus
 export LOTUS_PATH=/mnt/lotus-data
-$lotus_dir/lotus daemon --halt-after-import --import-snapshot  /mnt/latest.zst
+$lotus_dir/lotus daemon --halt-after-import --remove-existing-chain --import-snapshot  /mnt/latest.zst
