2702a3a600123b4b132f8e70fdccc5e17775b48d
Jiaying Wang <42981373+jennijuju@users.noreply.github.com>
2024-01-09 02:27:43 +0800

    Update download_snapshot.bash

--- setup/download_snapshot.bash
+++ setup/download_snapshot.bash
@@ -11,7 +11,7 @@ if [[ ! -e /mnt/latest.zst ]]; then
     # Determine the appropriate action based on the value of LOTUS_NETWORK
     if [ "$LOTUS_NETWORK" == "mainnet" ]; then
         # Download the latest mainnet snapshot using aria2
-        aria2c -x5 https://snapshots.mainnet.filops.net/minimal/latest.zst -o latest.zst --dir=/mnt/
+        aria2c -x5 https://forest-archive.chainsafe.dev/latest/mainnet/ -o latest.zst --dir=/mnt/
     elif [ "$LOTUS_NETWORK" == "calibnet" ]; then
         # Download the latest calibration net snapshot using aria2
         aria2c -x5 https://snapshots.calibrationnet.filops.net/minimal/latest.zst -o latest.zst --dir=/mnt/
